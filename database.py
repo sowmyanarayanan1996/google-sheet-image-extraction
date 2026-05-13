a30d24071321f7cca2b4ff974dcbce33722ea518
sowmyanarayanan <sowmyanarayanan1996@gmail.com>
2026-05-13 22:02:42 +0530

    resolved the database file and errors

---
+++ database.py
@@ -0,0 +1,66 @@
+import sqlite3
+import os
+
+# ==========================================
+# CREATE DATA DIRECTORY
+# ==========================================
+
+os.makedirs(
+    "data",
+    exist_ok=True
+)
+
+# ==========================================
+# SQLITE CONNECTION
+# ==========================================
+
+conn = sqlite3.connect(
+    "data/ocr_database.db",
+    check_same_thread=False
+)
+
+cursor = conn.cursor()
+
+# ==========================================
+# CREATE TABLE
+# ==========================================
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS excel_history (
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    google_sheet_url TEXT,
+
+    excel_name TEXT,
+
+    total_records INTEGER,
+
+    created_at TEXT
+
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS ocr_records (
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    excel_history_id INTEGER,
+
+    image_name TEXT,
+
+    image_data BLOB,
+
+    extracted_text TEXT,
+
+    source_type TEXT,
+
+    image_url TEXT,
+
+    created_at TEXT
+
+)
+""")
+
+conn.commit()
